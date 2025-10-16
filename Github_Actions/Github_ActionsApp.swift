6605f5ac99dc502dc2aa91352090c7f53f96a04c
Tugberk Acabey <acabeytugberk@gmail.com>
2025-10-16 13:11:31 +0300

    Initial Commit

---
+++ Github_Actions/Github_ActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ActionsApp.swift
+//  Github_Actions
+//
+//  Created by Tuğberk Acabey on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Github_ActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
