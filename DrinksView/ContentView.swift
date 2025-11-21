32b470d0d573bcc2e848c825ed8559aa821c39a1
Tugberk Acabey <acabeytugberk@gmail.com>
2025-11-21 11:33:21 +0300

    test file updated

--- DrinksView/ContentView.swift
+++ DrinksView/ContentView.swift
@@ -6,8 +6,7 @@
 //
 
 import SwiftUI
-// test
-// test
+// test v2
 // FIRST PAGE SHOPPİNG
 struct ContentView: View {
     @State private var drinks = ["Coca-Cola", "Pepsi", "Sprite", "Dr. Pepper"]
