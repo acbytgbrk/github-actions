163ac630547e092c78024293681996334314f060
Tugberk Acabey <acabeytugberk@gmail.com>
2025-11-21 10:23:47 +0300

    update

--- DrinksView/ContentView.swift
+++ DrinksView/ContentView.swift
@@ -13,6 +13,7 @@ import SwiftUI
 // update auto merge.yml
 // update
 //update
+// updatee
 struct ContentView: View {
     @State private var drinks = ["Coca-Cola", "Pepsi", "Sprite", "Dr. Pepper"]
     
