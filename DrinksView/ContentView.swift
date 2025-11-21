e570babd57bde662b97c385323e4b19e57f61562
Tugberk Acabey <acabeytugberk@gmail.com>
2025-11-21 08:26:52 +0300

    trigger auto merge workflow

--- DrinksView/ContentView.swift
+++ DrinksView/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+// auto merge workflow test
 // FIRST PAGE SHOPPİNG
 struct ContentView: View {
     @State private var drinks = ["Coca-Cola", "Pepsi", "Sprite", "Dr. Pepper"]
