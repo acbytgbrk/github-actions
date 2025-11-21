1bc54b1158ac542f9aad966992a591fb12630522
Tugberk Acabey <acabeytugberk@gmail.com>
2025-11-21 08:54:55 +0300

    trigger synchronize event for auto merge

--- DrinksView/ContentView.swift
+++ DrinksView/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 // auto merge workflow test
 // FIRST PAGE SHOPPİNG
+// auto merge workflow test - synchronize trigger
 struct ContentView: View {
     @State private var drinks = ["Coca-Cola", "Pepsi", "Sprite", "Dr. Pepper"]
     
