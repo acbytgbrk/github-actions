0c603bc2995443771e9e1bb52647609def93bc54
Tugberk Acabey <acabeytugberk@gmail.com>
2025-11-21 10:35:15 +0300

    trigger v2

--- DrinksView/ContentView.swift
+++ DrinksView/ContentView.swift
@@ -6,14 +6,7 @@
 //
 
 import SwiftUI
-// auto merge workflow test
-// FIRST PAGE SHOPPİNG
-// auto merge workflow test - synchronize trigger
-// trigger auto merge after CI added
-// update auto merge.yml
-// update
-//update
-// updatee
+// auto merge workflow test v2
 struct ContentView: View {
     @State private var drinks = ["Coca-Cola", "Pepsi", "Sprite", "Dr. Pepper"]
     
